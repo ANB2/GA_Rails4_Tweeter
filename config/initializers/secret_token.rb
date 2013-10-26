4b654911c63d630ab4b3aece507a090e6a34ffb4
vagrant <vagrant@precise32.(none)>
2013-10-26 13:20:44 +0000

    Initial Commit 2013-10-26 14:21

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweeter::Application.config.secret_key_base = '4a06359d47c7ffb7711cb7f43c850f7407afedf3d750452fc3def203e3f62b2abbc9c26d6af86dff9a51c36a8a25d69fd28a13b77b185d68d671fa58aca054d1'
